a39d6bd53e62cfef4f20ed4da686fccfa33c6c85
canmeng12 <108603664+canmeng12@users.noreply.github.com>
2023-12-12 00:11:40 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -42,8 +42,12 @@ sed -i 's/1.9rem/1rem/' luci-theme-edge/htdocs/luci-static/edge/cascade.css
 sed -i 's@.*mediaurlbase*@#&@g' luci-theme*/root/etc/uci-defaults/30_luci-theme-*
 
 #adguardhome,bypass
-git clone --depth 1 https://github.com/kiddin9/openwrt-packages packages12 && mv -n packages12/{luci-app-adguardhome,luci-app-bypass} ./ && mv -n packages12/adguardhome ./packages/
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages packages12 && mv -n packages12/luci-app-bypass ./ && mv -n packages12/adguardhome ./packages/
 rm -rf packages12
+
+git clone --depth 1 https://github.com/Hyy2001X/AutoBuild-Packages packages12 && mv -n packages12/luci-app-adguardhome ./ 
+rm -rf packages12
+
 #sed -i 's@.*AdGuardHome.yaml*@#&@g' ./*adguardhome/Makefile
 #sed -i 's/AdGuardHome.yaml/config\/AdGuardHome.yaml/g' ./*adguardhome/root/etc/config/AdGuardHome
 #sed -i 's/default y/default n/g' ./*adguardhome/Makefile
